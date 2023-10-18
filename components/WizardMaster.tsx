8ba10ae6354c8ebe6a494ca329b769cf77c9a95b
8848digital <ritik@8848digital.com>
2023-10-18 11:45:04 +0530

    created wizard and progress bar

--- components/WizardMaster.tsx
+++ components/WizardMaster.tsx
@@ -1,22 +1,32 @@
-import React, { useState, useRef } from 'react';
-import { useDispatch } from 'react-redux';
-import { storeFormDataAction } from '@/store/slices/formAction_slice';
-import MultiStep from './StepsProgressIndicator';
-import Step1EnterEmail from './SignUp/PersonalInfo/Step1EnterEmail';
-import Step3EnterName from './SignUp/PersonalInfo/Step3EnterName';
-import Step2VarificationCode from './SignUp/PersonalInfo/Step2VarificationCode';
-import sideImg from '../public/assets/side-img.jpg'
-import StepWizard from 'react-step-wizard';
+import React, { useState, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { storeFormDataAction } from "@/store/slices/formAction_slice";
+import MultiStep from "./StepsProgressIndicator";
+import Step1EnterEmail from "./SignUp/PersonalInfo/Step1EnterEmail";
+import Step3EnterName from "./SignUp/PersonalInfo/Step3EnterName";
+import Step2VarificationCode from "./SignUp/PersonalInfo/Step2VarificationCode";
+import sideImg from "../public/assets/side-img.jpg";
+// import StepWizard from "react-step-wizard";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import MobileStepper from "@mui/material/MobileStepper";
+
+import styles from "@/styles/wizard.module.css";
+import { styled } from "@mui/material/styles";
+
 const WizardMaster = () => {
   const [currentStep, setCurrentStep] = useState(1);
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const dispatch = useDispatch();
 
   const [stepFormData, setStepFormData] = useState<any>({
-    email: '',
-    verificationCode: ['', '', '', ''], // Initialize an array for verification codes
-    firstName: '',
-    lastName: '',
-    phoneNumber: '',
+    email: "",
+    verificationCode: ["", "", "", ""], // Initialize an array for verification codes
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
   });
 
   const handleNext = () => {
@@ -48,13 +58,54 @@ const WizardMaster = () => {
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-4 p-0">
-          <div style={{ width: '380px' }}>
-            <img src={sideImg.src} alt="Your Image" style={{ width: '100%',height:'800px' }} />
+          <div style={{ width: "380px" }} className={styles.image_cont}>
+            <img
+              src={sideImg.src}
+              alt="Your Image"
+              style={{ width: "100%", height: "800px" }}
+            />
+            <div className={styles.wizard_cont}>
+              <Box sx={{ width: "100%" }}>
+                <Stepper activeStep={currentStep - 1} orientation="vertical">
+                  {data.map((e: number) => {
+                    return (
+                      <Step key={e} >
+                        <StepLabel></StepLabel>
+                      </Step>
+                    );
+                  })}
+                </Stepper>
+              </Box>
+            </div>
           </div>
         </div>
-        <div className="col-md-8">
-          <div>
+        <div className="col-md-8 col-lg-8 mt-5">
+          <div className="row ">
+            <div className="col-4">
+              <div>
+                <h2 className="fs-3">Step {currentStep}</h2>
+                <hr className={styles.step_hr} />
+              </div>
+            </div>
+            <div className="col-8 position-relative">
+              <div className={styles.progress_bar_div}>
+                <div className="">
+                  <p className="mb-4">{currentStep} of 10 completed</p>
+                </div>
 
+                <MobileStepper
+                  variant="progress"
+                  steps={11}
+                  backButton={<></>}
+                  nextButton={<></>}
+                  activeStep={currentStep}
+                  className={styles.progress_bar}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
             {currentStep === 1 && (
               <Step1EnterEmail
                 formData={stepFormData}
@@ -77,9 +128,12 @@ const WizardMaster = () => {
 
           <div className="row">
             <div className="col-12">
-              <div className="text-center" style={{ marginTop: '180px' }}>
+              <div className="text-center" style={{ marginTop: "180px" }}>
                 {currentStep > 1 && (
-                  <button className="btn btn-prev me-3" onClick={handlePrevious}>
+                  <button
+                    className="btn btn-prev me-3"
+                    onClick={handlePrevious}
+                  >
                     Previous
                   </button>
                 )}
