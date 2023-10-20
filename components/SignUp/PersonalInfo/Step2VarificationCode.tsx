861f2fa77677a4f6bd2a99ef7cfd255fa6a8370c
unknown <shadab@8848digital.com>
2023-10-20 17:07:00 +0530

    ui changes:SK

--- components/SignUp/PersonalInfo/Step2VarificationCode.tsx
+++ components/SignUp/PersonalInfo/Step2VarificationCode.tsx
@@ -8,12 +8,12 @@ interface Step2Props {
 }
 
 const Step2VarificationCode = ({ formData, onFormDataChange }: any) => {
-    const [verificationCodes, setVerificationCodes] = useState<any>('');
+    const [userPassword, setUserPassword] = useState<any>('');
 
     const handleVerificationCodeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newVerificationCode = e.target.value;
-        setVerificationCodes(newVerificationCode);
-        onFormDataChange('verificationCode', newVerificationCode);
+        const userPwd = e.target.value;
+        setUserPassword(userPwd);
+        onFormDataChange('verificationCode', userPwd);
     };
 
     return (
@@ -21,26 +21,26 @@ const Step2VarificationCode = ({ formData, onFormDataChange }: any) => {
             <div className={`card p-4 ${styles.common_wizard_wrapper}`} style={{ maxWidth: '800px', height: '300px' }}>
                 <div className="row">
                     <div className="col-12">
-                        <div className='text-center'>
+                        <div className='text-center mt-5'>
                             <h1>Step 2 of 7</h1>
                             <h2>Personal Information</h2>
                             <p>{formData.email}</p>
                             <div>
-                                <p>Enter Verification Code Below</p>
+                                <p>Enter Password</p>
                                 <div className='d-flex align-items-center justify-content-center'>
                                    
                                         <input
                                            
                                             className="form-control me-2"
-                                            type="text"
+                                            type="password"
                                             style={{ width: '150px' }}
-                                            value={verificationCodes}
+                                            value={userPassword}
                                             onChange={(e) => handleVerificationCodeChange(e)}
                                         />
                                    
                                 </div>
                             </div>
-                            <p className='mt-2'>Not received? <Link href=''>Re-send</Link></p>
+                            {/* <p className='mt-2'>Not received? <Link href=''>Re-send</Link></p> */}
                         </div>
                     </div>
                 </div>
