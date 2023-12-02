648e3b4aa138d1f060fde1490e94df2cb274f3b6
Shadab Khan <shadab@8848digital.com>
2023-12-02 11:50:40 +0530

    code merge with shadab-dev-new:sk

--- services/config/api-config.ts
+++ services/config/api-config.ts
@@ -1,5 +1,5 @@
 export const CONSTANTS =  {
-    API_BASE_URL : "https://staging-strategicgears.frappe.cloud",
+    API_BASE_URL : "https://strategicgears.frappe.cloud",
     API_MANDATE_PARAMS: "/api/method/sme_sg.sdk.api",
     VERSION:"v1"
 }
