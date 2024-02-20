3027d657c69c3dd057bc58e75e20e01ba5d2a3f6
Shadab Khan <shadab@8848digital.com>
2024-02-20 22:20:06 +0530

    service changes

--- components/LandingPage/Services/Services.tsx
+++ components/LandingPage/Services/Services.tsx
@@ -66,7 +66,7 @@ const Services = () => {
         <div className={`${styles.our_services_wrapper}`} data-aos="slide-up">
             <div className="row">
                 <div className="col-md-2">
-                    <div className="row ">
+                    <div className="row">
                         <div className="col-md-12 col-6">
                             <div className={`${styles.services_heading} `}>
                                 <h1>Our Service</h1>
