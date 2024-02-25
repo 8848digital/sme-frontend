b2748f70f3474ba97b0370fe72a40132e89aab41
Shadab Khan <shadab@8848digital.com>
2024-02-25 15:39:30 +0530

    fix:account issue fixed:sk

---
+++ pages/account.tsx
@@ -0,0 +1,13 @@
+import AccountMaster from '@/components/AccountView/Account/AccountMaster'
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      <AccountMaster/>
+    </div>
+  )
+}
+
+export default index
+
